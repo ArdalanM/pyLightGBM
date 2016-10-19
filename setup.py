a37268c9b4ce58e2cede4e2607e17b931c6e3024
ArdalanM <ardalan77400@gmail.com>
2016-10-19 23:10:25 +0200

    initial commit: two working examples

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='pylightgbm',
+      version='0.1',
+      description='Python wrapper of Microsoft LightGBM',
+      author='Ardalan MEHRANI',
+      author_email='ardalan77400@gmail.com',
+      url='https://github.com/ArdalanM/pyLightGBM',
+      download_url='https://github.com/ArdalanM/pyLightGBM',
+      license='MIT',
+      install_requires=[],
+      extras_require={},
+      packages=find_packages())
