8d83ebbb626b19369cbb61c084ecf6fd1a301dd9
ArdalanM <ardalan77400@gmail.com>
2016-10-20 14:03:05 +0200

    fix: setup install attempt at early stopping

--- examples/simple_regression.py
+++ examples/simple_regression.py
@@ -12,7 +12,8 @@ np.random.seed(1337) # for reproducibility
 X, y = datasets.load_diabetes(return_X_y=True)
 # 'exec_path' is the path to lightgbm executable
 clf = GBMRegressor(exec_path="~/Documents/apps/LightGBM/lightgbm",
-                   num_iterations=100, learning_rate=0.01, num_leaves=10, min_data_in_leaf=10)
+                   num_iterations=10000, learning_rate=0.01, num_leaves=10, min_data_in_leaf=10,
+                   early_stopping_round=10)
 
 
 x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=42)
