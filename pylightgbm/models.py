71cf71911f48e5b918b827c4665196a58616057e
Nikolay Sukhanov <tau0@tau0-devel.haze.yandex.net>
2016-10-21 18:08:57 +0300

    enable sklearn compatibility

--- pylightgbm/models.py
+++ pylightgbm/models.py
@@ -18,10 +18,11 @@ class genericGMB(BaseEstimator):
                  num_leaves=127, tree_learner="serial", num_threads=1,
                  min_data_in_leaf=100, metric='l2',
                  feature_fraction=1., feature_fraction_seed=2, bagging_fraction=1., bagging_freq=0, bagging_seed=3,
-                 metric_freq=1, early_stopping_round=0, ):
+                 metric_freq=1, early_stopping_round=0, model=None):
 
         self.exec_path = exec_path
         self.config = config
+        self.model = model
         self.param = {
             'application': application,
             'num_iterations': num_iterations,
@@ -41,62 +42,72 @@ class genericGMB(BaseEstimator):
         }
 
         # create tmp dir to hold data and model (especially the latter)
-        self.tmp_dir = tempfile.mkdtemp()
-        self.output_model = os.path.join(self.tmp_dir, "LightGBM_model.txt")
-        print("CREATE: {}".format(self.tmp_dir), file=sys.stderr)
-        self.param['output_model'] = self.output_model
 
     def fit(self, X, y):
-        train_filepath = os.path.abspath("{}/X.svm".format(self.tmp_dir))
+        tmp_dir = tempfile.mkdtemp()
+        train_filepath = os.path.abspath("{}/X.svm".format(tmp_dir))
         datasets.dump_svmlight_file(X, y, train_filepath)
 
 
         self.param['task'] = 'train'
         self.param['data'] = train_filepath
+        self.param['output_model'] = os.path.join(tmp_dir, "LightGBM_model.txt")
 
         calls = ["{}={}\n".format(k, self.param[k]) for k in self.param]
 
         if self.config == "":
-            conf_filepath = os.path.join(self.tmp_dir, "train.conf")
+            conf_filepath = os.path.join(tmp_dir, "train.conf")
             open(conf_filepath, 'w').writelines(calls)
             os.system("{} config={}".format(self.exec_path, conf_filepath))
         else:
             os.system("{} config={}".format(self.exec_path, self.config))
 
+        with open(self.param['output_model'], mode='rb') as file:
+            self.model = file.read()
+
+        shutil.rmtree(tmp_dir)
+
     def predict(self, X):
-        predict_filepath = os.path.abspath(os.path.join(self.tmp_dir, "X_to_pred.svm"))
-        conf_filepath = os.path.join(self.tmp_dir, "predict.conf")
-        output_results = os.path.abspath(os.path.join(self.tmp_dir, "LightGBM_predict_result.txt"))
+        tmp_dir = tempfile.mkdtemp()
+        predict_filepath = os.path.abspath(os.path.join(tmp_dir, "X_to_pred.svm"))
+        output_model = os.path.abspath(os.path.join(tmp_dir, "model"))
+        conf_filepath = os.path.join(tmp_dir, "predict.conf")
+        output_results = os.path.abspath(os.path.join(tmp_dir, "LightGBM_predict_result.txt"))
+        with open(output_model, mode="wb") as file:
+            file.write(self.model)
+
         datasets.dump_svmlight_file(X, np.zeros(len(X)), f=predict_filepath)
 
         calls = [
             "task = predict\n",
             "data = {}\n".format(predict_filepath),
-            "input_model = {}\n".format(self.output_model),
+            "input_model = {}\n".format(output_model),
             "output_result={}\n".format(output_results)
         ]
 
         open(conf_filepath, 'w').writelines(calls)
         os.system("{} config={}".format(self.exec_path, conf_filepath))
         y_pred = np.loadtxt(output_results, dtype=float)
+        shutil.rmtree(tmp_dir)
         return y_pred
 
-    def get_params(self, deep):
+    def get_params(self, deep=True):
         params = dict(self.param)
         params['exec_path'] = self.exec_path
         params['config'] = self.config
-        del params['output_model']
+        params['model'] = self.model
+        if 'output_model' in params:
+            del params['output_model']
         return params
 
-    def set_params(self, *args, **kwargs):
-        print(args)
-        print(kwargs)
+    def set_params(self, **kwargs):
+        params = self.get_params()
+        params.update(kwargs)
+        self.__init__(**params)
+        return self
 
     def __del__(self):
-        if self.tmp_dir:
-            print("REMOVE: {}".format(self.tmp_dir), file=sys.stderr)
-            shutil.rmtree(self.tmp_dir)
-            self.tmp_dir=None
+        pass
 
 class GBMRegressor(genericGMB, RegressorMixin):
      def __init__(self, *args, **kwargs):
