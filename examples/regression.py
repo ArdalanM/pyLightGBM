8ace29a69e3748ecc40bf1966fb6512b7a757511
ArdalanM <ardalan77400@gmail.com>
2016-10-23 03:19:36 +0200

    update examples add best round example

--- examples/regression.py
+++ examples/regression.py
@@ -9,23 +9,21 @@ from pylightgbm.models import GBMRegressor
 
 # Parameters
 seed = 1337
-
+nfolds = 5
+path_to_exec = "~/Documents/apps/LightGBM/lightgbm"
 
 np.random.seed(seed) # for reproducibility
 X, y = datasets.load_diabetes(return_X_y=True)
 x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=seed)
 
-
 # 'exec_path' is the path to lightgbm executable
-clf = GBMRegressor(exec_path="~/Documents/apps/LightGBM/lightgbm",
+clf = GBMRegressor(exec_path=path_to_exec,
                    num_iterations=10000, learning_rate=0.01,
                    num_leaves=10,
                    min_data_in_leaf=10,
-                   early_stopping_round=10)
+                   early_stopping_round=10, verbose=True)
 
 clf.fit(x_train, y_train, test_data=[(x_test, y_test)])
 y_pred = clf.predict(x_test)
-
 print("Mean Square Error: ", metrics.mean_squared_error(y_test, y_pred))
-
-
+print("Best round: ", clf.best_round)
