04c1d59b43a9cbf76d9c73cb7d79dfe51f94552f
Ardalan Mehrani <ardalan.mehrani@iosquare.com>
2016-10-27 16:34:43 +0200

    update example

--- examples/classification.py
+++ examples/classification.py
@@ -11,19 +11,19 @@ from pylightgbm.models import GBMClassifier
 # Parameters
 seed = 1337
 nfolds = 5
+test_size = 0.2
 path_to_exec = "~/Documents/apps/LightGBM/lightgbm"
-
 np.random.seed(seed)  # for reproducibility
 
-X, Y = datasets.make_classification(n_samples=100, n_features=100, random_state=seed)
-x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size=0.2, random_state=seed)
+X, Y = datasets.make_classification(n_samples=1000, n_features=100, random_state=seed)
+x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
 
 # 'exec_path' is the path to lightgbm executable
 clf = GBMClassifier(exec_path=path_to_exec,
-                    num_iterations=100, learning_rate=0.05,
-                    min_data_in_leaf=1,
+                    num_iterations=1000, learning_rate=0.01,
+                    min_data_in_leaf=1, num_leaves=5,
                     metric='binary_error',
-                    early_stopping_round=10)
+                    early_stopping_round=20)
 
 clf.fit(x_train, y_train, test_data=[(x_test, y_test)])
 
