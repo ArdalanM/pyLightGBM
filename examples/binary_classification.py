c4cda38521d0f8e2764d029dd2b7bcc8ba771540
Ardalan <ardsad>
2016-11-12 19:01:16 +0100

    add dart example

--- examples/binary_classification.py
+++ examples/binary_classification.py
@@ -16,18 +16,26 @@ np.random.seed(seed)  # for reproducibility
 X, Y = datasets.make_classification(n_samples=1000, n_features=100, n_classes=2, random_state=seed)
 x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size=0.2, random_state=seed)
 
-# 'exec_path' is the path to lightgbm executable
-clf = GBMClassifier(exec_path=path_to_exec,
-                    num_iterations=1000, learning_rate=0.01,
-                    min_data_in_leaf=1, num_leaves=5,
-                    metric='binary_error',
-                    early_stopping_round=20)
+params = {'exec_path': path_to_exec,
+          'num_iterations': 1000, 'learning_rate': 0.01,
+          'min_data_in_leaf': 1, 'num_leaves': 5,
+          'metric': 'binary_error', 'verbose': False,
+          'early_stopping_round': 20}
 
-clf.fit(x_train, y_train, test_data=[(x_test, y_test)])
+clfs = [
+    ['gbdt', GBMClassifier(boosting_type='gbdt', **params)],
+    ['dart', GBMClassifier(boosting_type='dart', drop_rate=0.02, drop_seed=4, **params)],
+]
 
-y_prob = clf.predict_proba(x_test)
-y_pred = y_prob.argmax(-1)
+for boosting_type, clf in clfs:
 
-print("Log loss: ", metrics.log_loss(y_test, y_prob))
-print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
-print("Best round: ", clf.best_round)
+    clf.fit(x_train, y_train, test_data=[(x_test, y_test)])
+    y_prob = clf.predict_proba(x_test)
+    y_pred = y_prob.argmax(-1)
+
+    print("booster {} loss: {}, accuracy: {}, best round: {}".format(
+        boosting_type,
+        metrics.log_loss(y_test, y_prob),
+        metrics.accuracy_score(y_test, y_pred),
+        clf.best_round
+    ))
